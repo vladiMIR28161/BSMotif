ff205a8cfa26995a295493dfbd982d0b10b10cdd
Vladimir <163292117+vladiMIR28161@users.noreply.github.com>
2026-03-11 00:35:29 +0700

    postprocessing.py

--- bsmotif/postprocessing.py
+++ bsmotif/postprocessing.py
@@ -4,17 +4,16 @@ import re
 
 def extract_min_branch_code(s):
     # Finds all numeric codes in curly brackets and returns the minimum (by hierarchy)
-    matches = re.findall(r"\{([\d\.]+)\}", str(s))
-    tuples = [tuple(map(int, m.split('.'))) for m in matches]
+    matches = re.findall(r"\{(.*?)\}", str(s))
+    tuples = [tuple((0, int(p)) if p.isdigit() else (1, p) for p in re.findall(r'\d+|[a-z]+', m)) for m in matches]
     return min(tuples) 
     
 def extract_branch_code(s):
     # Extracts the numeric code from the Branch column
-    m = re.search(r"\{([\d\.]+)\}", str(s))
-    return tuple(map(int, m.group(1).split('.')))
+    m = re.search(r"\{(.*?)\}", str(s))
+    return tuple((0, int(p)) if p.isdigit() else (1, p) for p in re.findall(r'\d+|[a-z]+', m.group(1)))
     
 def renumber_branch(branch_series):
     # reindexing of ordinal numbers within each branch
-    m = re.search(r"(\{[\d\.]+\})", branch_series)
-    branch_prefix = m.group(1)
-    return branch_prefix 
+    m = re.search(r"(\{.*?\})", branch_series)
+    return m.group(1)
